b9b57384b6ee37a098f0e5dd56258e58ab5bcbcf
Dave Gumba <davegumba@gmail.com>
2025-06-26 16:55:07 -0400

    Initial Commit

---
+++ AcidDrumz/AcidDrumzApp.swift
@@ -0,0 +1,17 @@
+//
+//  AcidDrumzApp.swift
+//  AcidDrumz
+//
+//  Created by Dave Gumba on 2025-06-26.
+//
+
+import SwiftUI
+
+@main
+struct AcidDrumzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AcidDrumzTests/AcidDrumzTests.swift
@@ -0,0 +1,17 @@
+//
+//  AcidDrumzTests.swift
+//  AcidDrumzTests
+//
+//  Created by Dave Gumba on 2025-06-26.
+//
+
+import Testing
+@testable import AcidDrumz
+
+struct AcidDrumzTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
