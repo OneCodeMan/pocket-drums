9ab75767f5f14b31338ad82ce56514b569719646
Dave Gumba <davegumba@gmail.com>
2025-06-30 16:23:50 -0400

    testing out autechre swaures

--- AcidDrumz/View/ADAutechreView.swift
+++ AcidDrumz/View/ADAutechreView.swift
@@ -5,13 +5,28 @@
 //  Created by Dave Gumba on 2025-06-30.
 //
 
+/**
+ grid of 16 cols like those Captain Pikant vids
+ */
+
 import SwiftUI
 
 struct ADAutechreView: View {
+    // 16 for 4 bars; 1 for labeling very left
+    let columns = Array(repeating: GridItem(.flexible()), count: 17)
+    let rows = Array(repeating: GridItem(.fixed(30)), count: 7)
     
     var body: some View {
-        VStack {
-            Text("Hii this is the Autechre view")
+        ScrollView {
+            LazyHGrid(rows: rows) {
+                ForEach(0...16, id: \.self) { item in
+                    // TODO: refactor to own class: "ADAutechreOneSquare"
+                    RoundedRectangle(cornerRadius: 2.0)
+                        .fill(Color.gray)
+                        .frame(width: 35, height: 35)
+                    
+                }
+            }
         }
     }
 }
